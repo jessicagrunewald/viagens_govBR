1626e56d4e2450513858d0b32d263781d3d4d38a
jessicagrunewald <j_grunewald@hotmail.com>
2024-05-20 06:58:14 +1200

    Criando uma nova coluna com os dados da coluna Período - Data de início no formato Date

--- viagens.R
+++ viagens.R
@@ -8,3 +8,12 @@ viagens <- read.csv2(
   check.names = F
 )
 
+library(dplyr)
+# Verificar informação sobre cada coluna
+glimpse(viagens)
+
+# Criando uma nova coluna com os dados da coluna Período - Data de início
+# no formato Date
+viagens$data.inicio <- as.Date(viajens$`Período - Data de início`,
+                               format = "%d/%m/%Y")
+
